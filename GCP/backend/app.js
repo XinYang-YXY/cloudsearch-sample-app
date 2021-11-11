93a8e61fb2737fb5ddd82921b907211077eec1b9
Yi Sheng Yap <yap_yi_sheng@tech.gov.sg>
2021-11-11 13:33:55 +0800

    Convert native mysql package to sequelize
    Sequelize is an ORM
    ORM provides models and basic security features
    It also enable auto data structure generation

--- GCP/backend/app.js
+++ GCP/backend/app.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const mysql = require("mysql");
 const cors = require('cors')
 const app = express();
+const db = require("./models");
 
 app.use(cors())
 app.use(express.json());
-require('dotenv').config()
-const pool = mysql.createPool({
-  // socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`, # For GCP
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+require('dotenv').config();
+
+// Initialize DB configuration with Sequelize
+// https://sequelize.org/
+db.sequelize.sync();
+const Subscribers = db.subscribers;
 
 app.get("/", async (req, res) => {
   res.json({status: "success", message: "Welcome to the CloudSeArch sample app backend!"})
@@ -21,33 +19,35 @@ app.get("/", async (req, res) => {
 app.post("/subscribe", async (req, res) => {
   const userEmail = req.body.email;
   const userName = req.body.name;
-  const data = {
+  
+  Subscribers.create({
     email: userEmail,
     name: userName,
-  };
-  const query = "INSERT INTO subscriber VALUES (?, ?)";
-  pool.query(query, Object.values(data), (error) => {
-    if (error) {
-      res.json({ status: "failure", reason: error.code });
-    } else {
-      const msg = `[CREATED] ${userEmail} - ${userName}`;
-      console.log(msg);
-      res.json({ status: "success", data: data });
-    }
+  })
+  .then(data => {
+    const msg = `[CREATED] ${data.email} - ${data.name}`;
+    console.log(msg);
+    res.json({ status: "success", data: data });
+
+    res.send(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({ status: "failure", reason: err.message });
   });
 });
 
 app.get("/subscribe/count", async (req, res) => {
-  const query = "SELECT COUNT(*) as totalSubscriber from newsletter.subscriber";
-  pool.query(query, [], (error, results) => {
-    if (!results[0]) {
-      res.json({ status: "failure" });
-    } else {
-      const totalSubscriber = results[0].totalSubscriber
-      const msg = `[RETRIEVED] ${totalSubscriber}`;
-      console.log(msg);
-      res.json(totalSubscriber);
-    }
+  Subscribers.count()
+  .then(data => {
+    const msg = `[RETRIEVED] ${data}`;
+    console.log(msg);
+    res.json(data);
+    res.send(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({ status: "failure", reason: err.message });
   });
 });
 
@@ -58,35 +58,49 @@ app.put("/subscribe/update", (req, res) => {
     newEmail: userNewEmail,
     oldEmail: userOldEmail,
   };
-  const query = "UPDATE subscriber SET email = ? WHERE email = ?;";
-  pool.query(query, Object.values(data), (error, results) => {
-    if (error) {
-      res.json({ status: "failure", reason: error.code });
-    } else if (results.affectedRows === 0) {
+
+  Subscribers.update({ email: userNewEmail }, {
+    where: {
+      email: userOldEmail
+    }
+  })
+  .then(results => {
+    console.log(results);
+    if (results[0] === 0) {
       res.json({ status: "failure", reason: 400 });
-    } 
-    else {
+    } else {
       const msg = `[UPDATED] ${userOldEmail} -> ${userNewEmail}`;
       console.log(msg);
-      console.log(results)
       res.json({ status: "success", data: data });
     }
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({ status: "failure", reason: err.message });
   });
 });
 
 app.delete("/unsubscribe", (req, res) => {
   const userEmail = req.body.email;
-  const query = "DELETE FROM subscriber WHERE email = ?";
-  pool.query(query, [userEmail], (error, results) => {
-    if (error) {
-      res.json({ status: "failure", reason: error.code });
-    } else if (results.affectedRows === 0) {
+
+  Subscribers.destroy({
+    where: {
+      email: userEmail
+    }
+  })
+  .then(results => {
+    console.log(results);
+    if (results === 0) {
       res.json({ status: "failure", reason: 400 });
     } else {
       const msg = `[DELETED] ${userEmail}`;
       console.log(msg);
       res.json({ status: "success", data: userEmail });
     }
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({ status: "failure", reason: err.message });
   });
 });
 
