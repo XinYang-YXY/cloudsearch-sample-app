35a3868acf5e80589cfe3f2f4a9009510a14fdea
XinYang-YXY <me@yxy.ninja>
2021-12-08 07:13:02 +0800

    👌IMPROVE: Add in secrets manager for AWS

--- backend/config/smInit.js
+++ backend/config/smInit.js
@@ -1,33 +1,38 @@
 fs = require("fs");
 // Import the Secret Manager client and instantiate it:
-const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
-const client = new SecretManagerServiceClient();
-const secretPath = "projects/593463850334/secrets/db_pass"; // Project for which to manage secrets.
+var AWS = require("aws-sdk"),
+	region = "ap-northeast-1",
+	secretName =
+		"arn:aws:secretsmanager:ap-northeast-1:102153735617:secret:cloudsearch/db-iO8jhV",
+	secret;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+	region: region,
+});
 
 async function smInit() {
-	client
-		.accessSecretVersion({
-			name: secretPath + "/versions/1",
-		})
-		.then((data) => {
-			let dbPassword = data[0].payload.data.toString();
-			fs.readFile(".env", async (err, data) => {
-				if (err) throw err;
-				if (data.includes(`DB_PASS=${dbPassword}`)) {
-					console.log("✅ Secrets are already retrieved");
-				} else {
-					fs.appendFile(".env", `\nDB_PASS=${dbPassword}`, (err) => {
-						if (err) return console.log(err);
-					});
-					console.log(
-						"🎉 Data retrieved from Secrets Manager successfully!"
-					);
-				}
-			});
-		})
-		.catch((err) => {
-			console.log(err);
+	client.getSecretValue({ SecretId: secretName }, function (err, data) {
+		if ("SecretString" in data) {
+			secret = data.SecretString;
+			//console.log("SecretString retrieved = " + JSON.parse(secret).password) //working
+		}
+		console.log("SecretString retrieved = " + JSON.parse(secret).password); //working
+		let dbPassword = JSON.parse(secret).password;
+		fs.readFile(".env", async (err, data) => {
+			if (err) throw err;
+			if (data.includes(`DB_PASS=${dbPassword}`)) {
+				console.log("✅ Secrets are already retrieved");
+			} else {
+				fs.appendFile(".env", `\nDB_PASS=${dbPassword}`, (err) => {
+					if (err) return console.log(err);
+				});
+				console.log(
+					"🎉 Data retrieved from Secrets Manager successfully!"
+				);
+			}
 		});
+	});
 }
 
 smInit();
